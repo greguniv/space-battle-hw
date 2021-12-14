16e3e8f2c078aa163223df669c61db9c7e03faed
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2021-12-14 15:39:38 -0600

    redid js entirely - simplified into 2 objects and the start of a turn

--- script/spacebattle.js
+++ script/spacebattle.js
@@ -1,73 +1,52 @@
-//Setting up the Spaceship - main for everyone
-
-class Spaceship {
-    constructor(name, hull, firepower, accuracy) {
-        this.name = name
-        this.hull = hull
-        this.firepower = firepower
-        this.accuracy = accuracy
-    }
-}
-
-// ========== this is the Player Character ship ==========
-
-class Hero extends Spaceship {
-    constructor(name, hull, firepower, accuracy) {
-        super(name, hull, firepower, accuracy)
-    }
-    attack() {
-        console.log("Alien has attacked!")
-        usshw.hull -= this.firepower;
-        if (usshw.hull > 0) {
-            console.log(`You have taken damage! Current hull strength has dropped to ${usshw.hull}!`);
-        } else {;
-            console.log(usshw.death);
-        }
-    }
-    retreat() {
-        console.log("You have run away to live another day. But THEY are still out there...")
-    }
-    death() {
-        console.log("You have died. GAME OVER")
-    }
-}
+//  =================== re-do ==============================
+console.log("Ready to deploy.")
 
 
-// =============Alien here extended from spaceship ===================
-
-class Alien extends Spaceship {
-    constructor(name, hull, firepower, accuracy) {
-        super(name, hull, firepower, accuracy)
-    }
-    attack() {
-        console.log("You attacked the alien!")
-        Alien.hull -= this.firepower;
-        if (Alien.hull > 0) {
-            console.log(`They have taken damage! Their current hull strength has dropped to ${Alien.hull}!`);
+const hero = {
+    name: "USS Hello-World",
+    hull: 20,
+    firepower: 5,
+    accuracy: .7,
+    attack (target) {
+        let acc = Math.random();
+        if (acc < this.accuracy) {
+            console.log("Wow, direct hit!");
+            target.hull = target.hull - this.firepower;
+            console.log(`Enemy has ${target.hull} hit points left!`);
+            if(target.hull <= 0) {
+                console.log("You destroyed this enemy!")
+            }
         } else {
-            console.log("This alien has been destroyed.");
-            console.log(Alien.death);
-        }
-    }
-    death() {
-        console.log("The Alien has died.")
-    };
-}
-
-
-// ============ THE PLAYERS =================
-const enemyOne = new Alien('Roswell', '4', '3', '.6') //should be randomized*6?
-const usshw = new Spaceship('USS Hello World', '20', '5', '.7')
-// console.log(usshw)
-
-
-// ============= TURNS? ========
-
-let battle = (usshw, Alien) => {
-    while (usshw.hull > 0) {
-        usshw.attack(Alien);
-        if(Alien.hull > 0) {
-            Alien.attack(usshw);
+            console.log("Your shot missed! Prepare for evasive maneuvers!")
         }
     }
 }
+
+const enemy = {
+    hull: Math.round(Math.random() *(6-3) + 3),
+    firepower: Math.round(Math.random()*(4-2) + 2),
+    accuracy: (Math.random() * (.8 - .6) + .6),
+    attack(target) {
+        let acc = Math.random();
+        if (acc < this.accuracy) {
+            console.log("F");
+            target.hull = target.hull - this.firepower;
+            console.log(`The Hero has ${target.hull} hit points left!`);
+            if(target.hull <= 0) {
+                console.log("The Hero has been destroyed. The way to Earth is clear. Prepare for harvesting.") //this is game over
+            }
+        } else {
+            console.log("Evasive maneuvers successful!")
+        }
+    }
+}
+
+const battlesim = (player, computer) => {
+    while (hero.hull && enemy.hull > 0) {
+        player.attack(computer);
+        if(enemy.hull > 0) {
+            computer.attack(player);
+        }
+    }
+}
+
