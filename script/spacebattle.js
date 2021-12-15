5670103c89e2b6d00b340078c65de446836fff6a
GREGORY HERNANDEZ UNIVERSE <MAICMP8@HOTMAIL.COM>
2021-12-15 14:03:02 -0600

    added 2s wait on 2nd prompt to retreat or attack again

--- script/spacebattle.js
+++ script/spacebattle.js
@@ -1,23 +1,29 @@
 // =================================== DOM QUERIES
 // const enemyStats = document.querySelector("#enemyStats")
 
-
 console.log("Ready to deploy.")
 
+let game = true;
 
 const hero = {
     name: "USS Hello-World",
     hull: 20,
     firepower: 5,
     accuracy: .7,
-    attack (target) {
+    attack(target) {
         let acc = Math.random();
         if (acc < this.accuracy) {
             console.log("Wow, direct hit!");
             target.hull = target.hull - this.firepower;
             console.log(`Enemy has ${target.hull} hit points left!`);
-            if(target.hull <= 0) {
-                console.log("You destroyed this enemy! Retreat?")
+            if (target.hull <= 0) {
+                // console.log("You destroyed this enemy! Retreat?");
+                update();
+                // const userInput = prompt("You destroyed this enemy! Retreat?")
+                // if (userInput === 'yes') {
+                //     console.log("We will retreat and live to see another day.");
+                //     location.reload();
+                // }
             }
         } else {
             console.log("Your shot missed! Prepare for evasive maneuvers!")
@@ -25,17 +31,30 @@ const hero = {
     }
 }
 
+//UPDATE DISPLAY
+function update() {
+    document.getElementsByTagName('h1')[0].innerText = 2;
+    setTimeout( () => {
+        const userInput = prompt("You destroyed this enemy! Retreat?")
+        if (userInput === 'yes') {
+            console.log("We will retreat and live to see another day.");
+            location.reload();
+        }
+    }, 2000);
+}
+
+
 const enemy = {
-    hull: Math.round(Math.random() *(6-3) + 3),
-    firepower: Math.round(Math.random()*(4-2) + 2),
+    hull: Math.round(Math.random() * (6 - 3) + 3),
+    firepower: Math.round(Math.random() * (4 - 2) + 2),
     accuracy: (Math.random() * (.8 - .6) + .6),
-    
+
     attack(target) {
         let acc = Math.random();
         if (acc < this.accuracy) {
             target.hull = target.hull - this.firepower;
             console.log(`The Hero has ${target.hull} hit points left!`);
-            if(target.hull <= 0) {
+            if (target.hull <= 0) {
                 console.log("The Hero has been destroyed. The way to Earth is clear. Prepare for harvesting.") //this is game over?
             }
         } else {
@@ -44,15 +63,31 @@ const enemy = {
     }
 }
 
+let test = prompt("Earth is being attacked. Hero, will you help defend Earth?");
+console.log(test);
 
-//   function (hero, enemy) {
-//     let i = 0;
-//     while (i < hero.hull && enemy[1].hull > 0) {
-//         player.attack(computer);
-//         if(enemy.hull > 0) {
-//             computer.attack(player);
-//         }
-//     }
+const battle = function (hero, enemy) {
+    let i = 0;
+    while (hero.hull > 0 && enemy.hull > 0) {
+        hero.attack(enemy);
+        if (enemy.hull > 0) {
+            enemy.attack(hero);
+        }
+    }
+}
+
+
+// const aliens = [];
+
+// for(let i = 0; i < 6; i++) {
+//     aliens[i];
 // }
+
 console.log(hero)
 console.log(enemy)
+
+if (test === 'yes') {
+    battle(hero, enemy);
+    console.log('battle');
+    // if()
+}
